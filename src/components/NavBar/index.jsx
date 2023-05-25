7329b3d456cb0ec027ccef01f4cd600f11b6e27b
kenenx <kenen1996@hotmail.co.uk>
2023-05-25 10:24:22 +0100

    add navbar

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom';
+
+const NavBar = () => {
+
+  const styles = ({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' });
+  
+  
+  return (
+    <>
+      <header>
+        <nav>
+          <NavLink to="/" style={styles}>Home</NavLink>
+          <NavLink to="/superheros" style={styles}>View Heros</NavLink>
+          <NavLink to="/play" style={styles}>Play</NavLink>
+        </nav>
+      </header>
+      <Outlet/>
+    </>
+  )
+}
+
+export default NavBar
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as HomePage } from './HomePage'
+export { default as Trumps } from './Trumps'
+export { default as ViewAll } from './ViewAll'
