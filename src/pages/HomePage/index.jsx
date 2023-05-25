b09bb7bb2e35ec0a8dc1f81e9f217bba36aa6f1b
kenenx <kenen1996@hotmail.co.uk>
2023-05-25 14:29:20 +0100

    add instructions

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -5,6 +5,8 @@ const HomePage = () => {
     <div>
       <a>
         <img src="gokuvssuperman.png" className="logo" alt="Goku vs Superman" />
+        <h2>How to play:</h2>
+        <p>Select the powerstate that will beat your opponent</p>
       </a>
     </div>
   )
