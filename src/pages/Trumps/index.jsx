963be73aa682e9ea6b06a13001524e59bf92402f
kenenx <kenen1996@hotmail.co.uk>
2023-05-25 11:22:31 +0100

    add loading

--- src/pages/Trumps/index.jsx
+++ src/pages/Trumps/index.jsx
@@ -4,6 +4,7 @@ import { Card, ListGroup } from 'react-bootstrap';
 const Trumps = () => {
 
     const [superhero, setSuperhero] = useState("");
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetchSuperheroes()
@@ -15,30 +16,37 @@ const Trumps = () => {
             const data = await response.json()
             console.log(data)
             setSuperhero(data)
+            setLoading(false)
+            // console.log(superhero.image.url)
         } catch (error) {
             console.log('error message:', error);
         }
     }
 
+
+
     return (
         <>
             <div>Trumps Card</div>
+            {
+              loading
+              ? <p>Loading...</p>
+              : <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={superhero.image.url} />
 
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={superhero.image.url} />
-
-                <Card.Body>
-                    <Card.Title>{superhero.name}</Card.Title>
-                    <Card.Text>Powerstats</Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroup.Item>Intelligence: {superhero.powerstats.intelligence}</ListGroup.Item>
-                    <ListGroup.Item>Strength: {superhero.powerstats.strength}</ListGroup.Item>
-                    <ListGroup.Item>Speed: {superhero.powerstats.speed}</ListGroup.Item>
-                    <ListGroup.Item>Durability: {superhero.powerstats.durability}</ListGroup.Item>
-                    <ListGroup.Item>Combat: {superhero.powerstats.combat}</ListGroup.Item>
-                </ListGroup>
-            </Card>
+              <Card.Body>
+                  <Card.Title>{superhero.name}</Card.Title>
+                  <Card.Text>Powerstats</Card.Text>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                  <ListGroup.Item>Intelligence: {superhero.powerstats.intelligence}</ListGroup.Item>
+                  <ListGroup.Item>Strength: {superhero.powerstats.strength}</ListGroup.Item>
+                  <ListGroup.Item>Speed: {superhero.powerstats.speed}</ListGroup.Item>
+                  <ListGroup.Item>Durability: {superhero.powerstats.durability}</ListGroup.Item>
+                  <ListGroup.Item>Combat: {superhero.powerstats.combat}</ListGroup.Item>
+              </ListGroup>
+          </Card>
+            }
         </>
     )
 }
